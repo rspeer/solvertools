358fc71306b6dab652d34095594382cc484755b1
Rob Speer <rob@luminoso.com>
2015-01-10 19:55:43 -0500

    make clue searching work

--- solvertools/search.py
+++ solvertools/search.py
@@ -2,20 +2,37 @@ from solvertools.normalize import alpha_slug
 from solvertools.util import data_path
 from whoosh.index import open_dir
 from whoosh import qparser
+import re
 
 INDEX = None
 QUERY_PARSER = None
 
 
-def search(q):
+def search(query, pattern=None, length=None, num=20):
     global INDEX, QUERY_PARSER
+    if pattern is not None:
+        pattern = pattern.lstrip('^').rstrip('$').lower()
+        pattern = re.compile('^' + pattern + '$')
+
     if INDEX is None:
         INDEX = open_dir(data_path('search'))
-        QUERY_PARSER = qparser.MultifieldParser(
-            ["text", "definition"], schema=INDEX.schema,
+        QUERY_PARSER = qparser.SimpleParser(
+            "definition", schema=INDEX.schema,
             group=qparser.OrGroup.factory(0.9)
         )
 
+    matches = []
     with INDEX.searcher() as searcher:
-        results = searcher.search(QUERY_PARSER.parse(q))
-        return [(result['text'], result['definition']) for result in results[:10]]
+        results = searcher.search(QUERY_PARSER.parse(query), limit=None)
+        seen = set()
+        for result in results:
+            text = result['text']
+            slug = alpha_slug(text)
+            if slug not in seen:
+                if length is None or length == len(slug):
+                    if pattern is None or pattern.match(slug):
+                        seen.add(slug)
+                        matches.append((text, result['definition']))
+                        if len(matches) >= num:
+                            break
+        return matches

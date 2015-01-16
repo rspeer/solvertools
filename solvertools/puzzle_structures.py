3dcbbbc3652263c8f2407fd1dfd6fff3e2762e60
Rob Speer <rob@luminoso.com>
2015-01-16 13:08:39 -0500

    use cromulence in more things

--- solvertools/puzzle_structures.py
+++ solvertools/puzzle_structures.py
@@ -116,28 +116,28 @@ def brute_force_diagonalize(answers, wordlist=WORDS, quiet=False):
 
         >>> metas = ['benjamins', 'billgates', 'donors', 'luxor', 'mansion', 'miserly', 'realty']
         >>> brute_force_diagonalize(metas)[0]   # doctest: +NORMALIZE_WHITESPACE
-        Log prob.   Cromulence  Text    Info
-        -19.4797    15.3    MENOROT 
-        -19.8101    14.9    BE NOISY    
-        -19.9530    14.8    DELLROY 
-        -20.3282    14.4    RUN EAST    
-        -21.3154    13.3    MIX LAST    
-        -21.3333    13.3    MAX LAST    
-        -21.4507    13.2    LES LIST    
-        -21.4619    13.1    RUN SALT    
-        -21.5178    13.1    LAS LAST    
-        -21.5394    13.1    BOX STAY    
-        -21.6241    13.0    MENOGYN 
-        -22.2569    12.3    MALORY I    
-        -22.3489    12.2    BUS LIST    
-        -22.4141    12.1    LINE TO I   
-        -22.6454    11.9    LIN LAST    
-        -22.9501    11.5    DEAL ROY    
-        -22.9835    11.5    ME NOT AN   
-        -23.2088    11.2    RUNS RAY    
-        -23.2311    11.2    MUS LAST    
-        -23.3860    11.1    BE SO IST   
-        (-19.479680042263826, 'MENOROT', None)
+        Cromulence  Text    Info
+        15.3    MENOROT 
+        14.9    BE NOISY    
+        14.8    DELLROY 
+        14.4    RUN EAST    
+        13.3    MIX LAST    
+        13.3    MAX LAST    
+        13.2    LES LIST    
+        13.1    RUN SALT    
+        13.1    LAS LAST    
+        13.1    BOX STAY    
+        13.0    MENOGYN 
+        12.3    MALORY I    
+        12.2    BUS LIST    
+        12.1    LINE TO I   
+        11.9    LIN LAST    
+        11.5    ME NOT AN   
+        11.5    DEAL ROY    
+        11.2    RUNS RAY    
+        11.2    MUS LAST    
+        11.1    BE SO IST   
+        (15.3, 'MENOROT', None)
 
     Okay, it actually thinks the best answer is "MENOROT", the plural of
     "menorah". But BE NOISY is in second place. And if that doesn't work,
@@ -153,7 +153,7 @@ def brute_force_diagonalize(answers, wordlist=WORDS, quiet=False):
             diag = diagonalize(permutation)
         except IndexError:
             continue
-        found = wordlist.search(diag, count=1)
+        found = wordlist.search(diag, count=1, use_cromulence=True)
         if found:
             logprob, text = found[0]
             slug = slugify(text)
